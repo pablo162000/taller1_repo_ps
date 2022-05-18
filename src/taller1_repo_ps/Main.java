4ea4e9dddbe099d8e9dbc9d10327fc634a500c9d
pbsun <pbsun@LAPTOP-0NBRK4L8>
2022-05-17 23:05:00 -0500

    Archivos necesarios para el proyecto

---
+++ src/taller1_repo_ps/Main.java
@@ -0,0 +1,10 @@
+package taller1_repo_ps;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto JAVA en GitHub");
+	}
+
+}
