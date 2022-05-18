a13003c66c08b72b3ef0aaca99e62516fa238f57
pbsun <pbsun@LAPTOP-0NBRK4L8>
2022-05-17 23:10:54 -0500

    Segundo cambio solictado

--- src/taller1_repo_ps/Main.java
+++ src/taller1_repo_ps/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi primer proyecto JAVA en GitHub");
+		System.out.println("Mi segundo comentario");
+
 	}
 
 }
